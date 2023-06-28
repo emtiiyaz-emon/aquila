ecec683786434ee190a2857e0f6eb11ad410767c
emtiiyaz-emon <emtiiyazemon@gmail.com>
2023-06-28 23:39:03 +0600

    first

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Main Template File.
+ * 
+ * @package Emon
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress Theme - Emon</title>
+</head>
+<body>
+    
+</body>
+</html>
